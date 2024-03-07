ba3d581f1d77f23a345d4f7b15e15845174b1a01
Usuario <Usuario@CPD02-M16>
2024-03-07 14:36:03 -0300

    MainPage.xaml finalizado

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,22 +7,7 @@ namespace MauiListaCompras
         static SQLiteDatabaseHelper _db;
         public static SQLiteDatabaseHelper Db
         {
-            get
-            {
-                if (_db == null)
-                {
-                    string path = Path.Combine(
-                        Environment.GetFolderPath(
-                            Environment.SpecialFolder.LocalApplicationData
-                        ), "banco_sqlite_compras.db3"
-                    );
-
-                    _db = new SQLiteDatabaseHelper(path);
-
-                }
-
-                return _db;
-            }
+            get { if (_db == null) { string path = Path.Combine( Environment.GetFolderPath( Environment.SpecialFolder.LocalApplicationData ), "banco_sqlite_compras.db3" );  _db = new SQLiteDatabaseHelper(path);  }  return _db; }
         }
         public App()
         {

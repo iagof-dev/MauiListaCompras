bd8e497807547c6cd788d76c54af6005386a5257
Usuario <Usuario@CPD02-M16>
2024-03-07 14:15:18 -0300

    SqliteDabaseHelper.cs finalizado

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,29 @@
-﻿namespace MauiListaCompras
+﻿using MauiListaCompras.Helpers;
+
+namespace MauiListaCompras
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData
+                        ), "banco_sqlite_compras.db3"
+                    );
+
+                    _db = new SQLiteDatabaseHelper(path);
+
+                }
+
+                return _db;
+            }
+        }
         public App()
         {
             InitializeComponent();
--- Helpers/SQLiteDatabaseHelper.cs
+++ Helpers/SQLiteDatabaseHelper.cs
@@ -31,5 +31,16 @@ namespace MauiListaCompras.Helpers
             return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
         }
 
+        public Task<List<Produto>> GetAll()
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+
+        public Task<List<Produto>> Search(string q)
+        {
+            string sql = "SELECT * FROM Produto WHERE descricao LIKE '%" + q + "%';";
+            return _conn.QueryAsync<Produto>(sql);
+        }
+
     }
 }
